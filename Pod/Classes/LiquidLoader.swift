46dc884e162a298d98aaa192fea2d72c837483d1
Weilson Wonder <weilson@live.com>
2016-04-13 11:30:50 +0800

    -

--- Pod/Classes/LiquidLoader.swift
+++ Pod/Classes/LiquidLoader.swift
@@ -40,6 +40,8 @@ public class LiquidLoader : UIView {
     public init(frame: CGRect, effect: Effect) {
         self.effect = effect
         super.init(frame: frame)
+        self.hidden = true
+        self.userInteractionEnabled = false
         self.effectDelegate = self.effect.setup(self)
     }
 
@@ -57,3 +59,148 @@ public class LiquidLoader : UIView {
         self.hidden = true
     }
 }
+
+public class LiquidLoaderFull : UIView {
+    public let liquidLoader: LiquidLoader
+    public var animationDuration: NSTimeInterval = 0.3
+    
+    public init(frame: CGRect, effect: Effect) {
+        liquidLoader = LiquidLoader(frame: frame, effect: effect)
+        super.init(frame: UIScreen.mainScreen().bounds)
+        liquidLoader.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(liquidLoader)
+        addConstraint(NSLayoutConstraint(
+            item: liquidLoader,
+            attribute: NSLayoutAttribute.CenterX,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.CenterX,
+            multiplier: 1,
+            constant: 0)
+        )
+        addConstraint(NSLayoutConstraint(
+            item: liquidLoader,
+            attribute: NSLayoutAttribute.CenterY,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.CenterY,
+            multiplier: 1,
+            constant: 0)
+        )
+        backgroundColor = UIColor(white: 0.25, alpha: 0.25)
+        userInteractionEnabled = true
+        hidden = true
+        liquidLoader.show()
+        layoutIfNeeded()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        liquidLoader = LiquidLoader(frame: CGRect(x: 0, y: 0, width: 50, height: 50), effect: Effect.Circle(UIColor.whiteColor()))
+        super.init(coder: aDecoder)
+        liquidLoader.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(liquidLoader)
+        addConstraint(NSLayoutConstraint(
+            item: liquidLoader,
+            attribute: NSLayoutAttribute.CenterX,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.CenterX,
+            multiplier: 1,
+            constant: 0)
+        )
+        addConstraint(NSLayoutConstraint(
+            item: liquidLoader,
+            attribute: NSLayoutAttribute.CenterY,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: self,
+            attribute: NSLayoutAttribute.CenterY,
+            multiplier: 1,
+            constant: 0)
+        )
+        backgroundColor = UIColor(white: 0.25, alpha: 0.25)
+        userInteractionEnabled = true
+        hidden = true
+        liquidLoader.show()
+        layoutIfNeeded()
+    }
+    
+    public func show(completion: (Void -> Void)? = nil) {
+        layoutIfNeeded()
+        if hidden {
+            alpha = 0
+            hidden = false
+        }
+        UIView.animateWithDuration(
+            animationDuration,
+            delay: 0,
+            options: [UIViewAnimationOptions.AllowAnimatedContent, UIViewAnimationOptions.BeginFromCurrentState],
+            animations: { 
+                self.alpha = 1
+            },
+            completion: { _ in
+                completion?()
+        })
+    }
+    
+    public func hide(completion: (Void -> Void)? = nil) {
+        layoutIfNeeded()
+        UIView.animateWithDuration(
+            animationDuration,
+            delay: 0,
+            options: [UIViewAnimationOptions.AllowAnimatedContent, UIViewAnimationOptions.BeginFromCurrentState],
+            animations: {
+                self.alpha = 0
+            },
+            completion: { _ in
+                self.hidden = true
+                completion?()
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
